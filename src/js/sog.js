eaa6ca983fed4f2e28893d20709e9b211b57fa6d
songzigw <songzigw@163.com>
2016-05-07 22:50:49 +0800

    trigger

--- src/js/sog.js
+++ src/js/sog.js
@@ -60,10 +60,99 @@
     };
 
     // window.sog
-    var s = {};
+    var s = {
+        breakpoints : {
+            largescreen : [ 991, -1 ],
+            tabletscreen : [ 768, 990 ],
+            devicescreen : [ 420, 767 ],
+            sdevicescreen : [ 0, 419 ]
+        },
+
+        lastBreakpoint : null
+    };
     s.$pageContainer = $('.page-container');
     s.$sidebarMenu = $('> .sidebar-menu', s.$pageContainer);
     s.sidebarMenu = new SidebarMenu(s.$sidebarMenu);
+
+    $(window).resize(function() {
+        $(window).trigger('sog.resize');
+    });
+    $(window).on('sog.resize', function() {
+        trigger_resizable();
+    });
+
+    /*
+     * Main Function that will be called each time when the screen breakpoint
+     * changes
+     */
+    function resizable(breakpoint) {
+        var sb_with_animation;
+
+        // Large Screen Specific Script
+        if (is('largescreen')) {
+
+        }
+
+        // Tablet or larger screen
+        if (ismdxl()) {
+        }
+
+        // Tablet Screen Specific Script
+        if (is('tabletscreen')) {
+        }
+
+        // Tablet device screen
+        if (is('tabletscreen')) {
+            s.sidebarMenu.collapsed();
+        }
+
+        // Tablet Screen Specific Script
+        if (isxs()) {
+        }
+    }
+
+    /* Functions */
+
+    // Get current breakpoint
+    function get_current_breakpoint() {
+        var width = $(window).width(), breakpoints = s.breakpoints;
+
+        for ( var breakpont_label in breakpoints) {
+            var bp_arr = breakpoints[breakpont_label], min = bp_arr[0], max = bp_arr[1];
+
+            if (max == -1)
+                max = width;
+
+            if (min <= width && max >= width) {
+                return breakpont_label;
+            }
+        }
+
+        return null;
+    }
+
+    // Check current screen breakpoint
+    function is(screen_label) {
+        return get_current_breakpoint() == screen_label;
+    }
+
+    // Is xs device
+    function isxs() {
+        return is('devicescreen') || is('sdevicescreen');
+    }
+
+    // Is md or xl
+    function ismdxl() {
+        return is('tabletscreen') || is('largescreen');
+    }
+
+    // Trigger Resizable Function
+    function trigger_resizable() {
+        if (s.lastBreakpoint != get_current_breakpoint()) {
+            s.lastBreakpoint = get_current_breakpoint();
+            resizable(s.lastBreakpoint);
+        }
+    }
 
     window.sog = s;
 })(window, jQuery);
